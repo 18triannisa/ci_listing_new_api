a58ad6cabc7901f7b589702329b9f48460038d0e
triannisa <triannisa@alphatech.id>
2018-10-30 19:49:59 +0700

    update footer

--- src/menu/footer.js
+++ src/menu/footer.js
@@ -4,7 +4,7 @@ const Footer = () => {
     return (    
     <footer class="blockquote-footer bg-dark">
         <div class="container-fluid footer">
-        <span class="text-footer">Copyright<i class="far fa-copyright"></i> by Alpha Tech 2018</span>
+        <span class="text-footer">Copyright<i class="far fa-copyright"></i> by Alpha Tech 2018 Tria</span>
         </div>
     </footer>
     
